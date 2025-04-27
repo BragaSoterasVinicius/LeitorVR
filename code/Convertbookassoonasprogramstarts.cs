aff488e850623ba32b1c2732e9a55f7c8a000e0d
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-04-27 16:05:03 -0300

    Tirando uns comentários a fim de facilitar a leitura | Convertbookassoonasprogramstarts.cs

--- code/Convertbookassoonasprogramstarts.cs
+++ code/Convertbookassoonasprogramstarts.cs
@@ -24,16 +24,6 @@ public class Convertbookassoonasprogramstarts : MonoBehaviour
                 string result = javaClass.CallStatic<string>("callConvertMethod", pdfPath, outputDir);
                 debugReader.GetComponent<TMPro.TextMeshPro>().text += "Method to convert pdf finished..." + result;
             }
-            /* try
-             {
-
-                 using (AndroidJavaClass javaClass = new AndroidJavaClass("MyClass"))
-                 {
-                     debugReader.GetComponent<TMPro.TextMeshPro>().text += pdfPath + "||" + outputDir;
-                     string result = javaClass.CallStatic<string>("callConvertMethod", pdfPath, outputDir);
-                     debugReader.GetComponent<TMPro.TextMeshPro>().text += "provavelmente funcionou, segue resultado: " + result;
-                 }
-             }*/
         }
         catch (Exception e)
         {
@@ -42,39 +32,4 @@ public class Convertbookassoonasprogramstarts : MonoBehaviour
             debugReader.GetComponent<TMPro.TextMeshPro>().text += "Problem when calling pdfConverter: " + e.Message;
         }
     }
-
-    /*void convertaolivroemimgs(string pdfFilePath, string outputDirectory)
-    {
-        dynamic sys = Py.Import("sys");
-        sys.path.append(Application.dataPath);
-        Runtime.PythonDLL = Application.streamingAssetsPath + "/pythonebend/python312.dll";
-        Debug.Log("PythonDLL: " + Runtime.PythonDLL);
-        PythonEngine.Initialize();
-        using (Py.GIL())
-        {
-            var pythonScript = Py.Import("convertaolivroemimgs");
-            var message = new PyString(pdfFilePath);
-            var result = pythonScript.InvokeMethod("mainene", new PyObject[] { message });
-            Debug.Log(result);
-        }
-        /*if (!File.Exists(pdfFilePath))
-        {
-            Debug.LogError("PDF file not found: " + pdfFilePath);
-            return;
-        }
-
-        using (var document = PdfDocument.Load(pdfFilePath))
-        {
-            for (int page = 0; page < document.PageCount; page++)
-            {
-                using (var image = document.Render(page, 300, 300, PdfRenderFlags.CorrectFromDpi))
-                {
-                    string outputFileName = Path.Combine(outputDirectory, $"page-{page + 1}.png");
-                    image.Save(outputFileName, System.Drawing.Imaging.ImageFormat.Png);
-                    Debug.Log($"Saved page {page + 1} to {outputFileName}");
-                }
-            }
-        }
-        */
-
 }
